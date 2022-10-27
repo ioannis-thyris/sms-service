61b3d6f598884f8c8ad38739cca160d1a3f422cf
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-10-27 17:55:42 +0300

    Created a middleware for global error handling.

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                // Pass the request to the next delegate in the pipeline
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var error = new { context.Response.StatusCode, ex.Message };
+
+            string errorJson = JsonSerializer.Serialize(error);
+
+            await context.Response.WriteAsync(errorJson);
+        }
+
+    }
+}
