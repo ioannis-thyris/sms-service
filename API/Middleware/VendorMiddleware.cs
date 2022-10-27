da61e24ff0e61e2bee0be359fad02e283d05bec0
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-10-27 17:55:04 +0300

    Removed PhoneNumberParses and moved the logic to the VendorMiddleware.

--- API/Middleware/VendorMiddleware.cs
+++ API/Middleware/VendorMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.DataTransferObjects;
+using SmsVendors;
 using SmsVendors.Contracts;
 using SmsVendors.Factory;
 using System.Text;
@@ -8,29 +9,70 @@ namespace API.Middleware
 {
     public class VendorMiddleware
     {
-        private readonly IPhoneNumberParser _parser;
+        private Dictionary<string, Country> countryNumbers = new()
+        {
+            {"+30", Country.GR},
+            {"+357", Country.CY},
+        };
 
         private readonly RequestDelegate _next;
 
-        public VendorMiddleware(RequestDelegate next, IPhoneNumberParser parser)
+        public VendorMiddleware(RequestDelegate next)
         {
             _next = next;
-            _parser = parser;
         }
 
         public async Task InvokeAsync(HttpContext context, ISmsVendorFactory factory)
         {
             context.Request.EnableBuffering();
 
-            var countryCode = await _parser.GetCountry(context);
+            var country = await GetCountry(context);
 
-            var vendor = factory.Create(countryCode);
+            var vendor = factory.Create(country);
 
             context.Items["Vendor"] = vendor;
 
             // Pass the request to the next delegate in the pipeline
             await _next(context);
         }
+
+
+
+        private async Task<Country> GetCountry(HttpContext context)
+        {
+            var dto = await ReadRequestBody(context.Request.Body);
+            string countryTelCode;
+
+            for (int i = 2; i <= 4; i++)
+            {
+                countryTelCode = dto.Number.Substring(0, i);
+
+                if (countryNumbers.ContainsKey(countryTelCode))
+                    return countryNumbers[countryTelCode];
+            }
+
+            return default(Country); // Avoid adding a value to the enum with zero value.
+        }
+
+        private async Task<SmsDto> ReadRequestBody(Stream body)
+        {
+            // Leave the body open so the next middleware can read it.
+            using (var reader = new StreamReader(body, leaveOpen: true))
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var dto = await JsonSerializer.DeserializeAsync<SmsDto>(body, options);
+
+                // Reset the request body stream position so the next middleware can read it
+                body.Position = 0;
+
+                return dto;
+            }
+        }
+
 
     }
 }
