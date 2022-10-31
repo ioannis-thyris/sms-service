e3d86a128afada56373bf0227f71dd7f603cdd0b
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-10-31 11:17:28 +0200

    Added integration tests project.

---
+++ IntegrationTests/TestingWebAppFactory.cs
@@ -0,0 +1,41 @@
+﻿using Database.AppContext;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntegrationTests
+{
+    public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<Context>));
+
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                services.AddDbContext<Context>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<Context>();
+
+                    db.Database.EnsureCreated();
+                }
+
+
+            });
+        }
+    }
+}
